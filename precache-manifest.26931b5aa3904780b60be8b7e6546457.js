41d9fd30a48561473bc7125f729a569e81ba5e03
majd maihoub <majd.maihoub@meta-itech.com>
2022-02-06 07:33:21 +0200

    Updates

---
+++ precache-manifest.26931b5aa3904780b60be8b7e6546457.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59503a3f9286d221be2ea965c2bc433c",
+    "url": "/react-flow-builder-test/index.html"
+  },
+  {
+    "revision": "1c451af68b70f92e6cc8",
+    "url": "/react-flow-builder-test/static/js/2.87466658.chunk.js"
+  },
+  {
+    "revision": "121edf2f6e6f0f88c75dea4f4c973db5",
+    "url": "/react-flow-builder-test/static/js/2.87466658.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f49d7b12b1ed0cf16bc",
+    "url": "/react-flow-builder-test/static/js/main.5077b8ea.chunk.js"
+  },
+  {
+    "revision": "c642aecd59a381a1f90b",
+    "url": "/react-flow-builder-test/static/js/runtime-main.53b42291.js"
+  }
+]);
